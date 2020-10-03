47fe942ae905eab36750dccbebab98e0433f212c
Daniel Parra <dani.parra.90@gmail.com>
2020-10-03 12:04:16 +0200

    Add tests

---
+++ Assets/Code/Tests/MyTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class MyTest
+    {
+        [Test]
+        public void MyTestSimplePasses()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+    }
+}
